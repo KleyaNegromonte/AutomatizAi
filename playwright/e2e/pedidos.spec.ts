863fcd9ec5d079ed6d13d81ae243c6eb13d133dd
Kleya Negromonte <negromontekleya@gmail.com>
2026-08-14 14:52:46 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,21 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('Deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange - Preparar o teste
   await page.goto('http://localhost:5173/');
-
-  // Verifica se a página está carregada
-
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Verifica se a página de consulta de pedido está carregada
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-RCW98N');
-
   await page.getByTestId('search-order-button').click();
+
+  // Assert
+
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-RCW98N');
+  
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 
